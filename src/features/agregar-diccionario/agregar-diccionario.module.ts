3cf28394c58528195d05496418953ae3821f7bc5
Fabian Alvarez <fabian.alvarezch@gmail.com>
2020-07-08 15:47:00 -0600

    Vista para crear diccionario
    Sin estilos y sin conexion al backend

--- src/features/agregar-diccionario/agregar-diccionario.module.ts
+++ src/features/agregar-diccionario/agregar-diccionario.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormDiccionarioComponent } from './form-diccionario/form-diccionario.component';
 import { AgregarDiccionarioComponent } from './agregar-diccionario/agregar-diccionario.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
-import { AsociarCategoriasDiccionarioComponent } from './asociar-categorias-diccionario/asociar-categorias-diccionario.component';
 import { CatGramaticalService } from '../../core/services/cat-gramatical.service';
 import { AppRoutingModule } from '../../app/app-routing.module';
 
@@ -12,8 +10,6 @@ import { AppRoutingModule } from '../../app/app-routing.module';
 @NgModule({
   declarations: [
     AgregarDiccionarioComponent,
-    FormDiccionarioComponent,
-    AsociarCategoriasDiccionarioComponent
   ],
   imports: [
     CommonModule,
--- src/features/agregar-diccionario/agregar-diccionario/agregar-diccionario.component.ts
+++ src/features/agregar-diccionario/agregar-diccionario/agregar-diccionario.component.ts
@@ -1,77 +1,112 @@
-import { Component, OnInit } from '@angular/core';
-import { Diccionario } from '../../../core/models/diccionario';
-import { DiccionarioService } from '../../../core/services/diccionario.service';
-import { CatGrmDiccionario } from '../../../core/models/cat-grm-diccionario';
-import { SubGrmDiccionario } from '../../../core/models/sub-grm-diccionario';
-import { ComunicadorParaCategoriasAsociadasService } from '../../../core/services/comunicador-para-categorias-asociadas.service';
-import { ComunicadorParaDiccionarioCreadoService } from '../../../core/services/comunicador-para-diccionario-creado.service';
-import { Router } from '@angular/router';
-import { CatGrmDiccionarioService } from '../../../core/services/cat-grm-diccionario.service';
-import { SubGrmDiccionarioService } from '../../../core/services/sub-grm-diccionario.service';
-import { Subscription } from "rxjs";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-diccionario',
   templateUrl: './agregar-diccionario.component.html',
   styleUrls: ['./agregar-diccionario.component.sass']
 })
-export class AgregarDiccionarioComponent implements OnInit {
+export class AgregarDiccionarioComponent {
 
-  nuevoDiccionario: Diccionario;
-  categoriasAsociadas: CatGrmDiccionario[];
-  subcategoriasAsociadas: SubGrmDiccionario[];
+  diccionarioFormGroup: FormGroup;
 
-  cdcServiceSubscription: Subscription;
-  ccaServiceSubscription: Subscription;
+  constructor(private formBuilder: FormBuilder) {
+    // Creamos un form group que consta de tres partes
+    // 1. El diccionario
+    // 2. Sus categorias gramaticales y subcategorias
+    // 3. La informacion y marcacion
+    this.diccionarioFormGroup = this.formBuilder.group({
+      diccionario: this.formBuilder.group({
+        nombre: this.formBuilder.control(''),
+        tipo: this.formBuilder.control(''),
+        descripcion: this.formBuilder.control(''),
+      }),
 
-  constructor(private diccionarioService: DiccionarioService,
-              private cdcService: ComunicadorParaDiccionarioCreadoService,
-              private ccaService: ComunicadorParaCategoriasAsociadasService,
-              private catGrmDicService: CatGrmDiccionarioService,
-              private subGrmDicService: SubGrmDiccionarioService,
-              private router: Router) {
-    this.cdcServiceSubscription = cdcService.changeEmitted$.subscribe(nuevoDiccionario => {
-      this.onDiccionarioCreado(nuevoDiccionario);
-    });
+      catGramaticales: this.formBuilder.array([]),
 
+      informacion: this.formBuilder.group({
+        ortografica: this.formBuilder.array([]),
+        fonetica: this.formBuilder.array([]),
+        morfologica: this.formBuilder.array([]),
+        etimologica: this.formBuilder.array([]),
+      }),
 
-    this.ccaServiceSubscription = ccaService.changeEmitted$.subscribe(catSubAsociadas => {
-      this.onCatSubAsociadas(catSubAsociadas);
-      this.unsubscribe();
-    });
-  }
-
-  ngOnInit() {
-  }
-
-  onDiccionarioCreado(nuevoDiccionario: Diccionario) {
-    this.nuevoDiccionario = nuevoDiccionario;
-  }
-
-  onCatSubAsociadas(catSubAsociadas: any[]) {
-    this.diccionarioService.crear(this.nuevoDiccionario).subscribe(nuevoDiccionario => {
-      this.nuevoDiccionario = nuevoDiccionario;
-
-      this.categoriasAsociadas = catSubAsociadas[0];
-      for (var categoriaAsociada of this.categoriasAsociadas) {
-        categoriaAsociada.diccionarioId = this.nuevoDiccionario.id;
-      }
-
-      this.catGrmDicService.crear(this.categoriasAsociadas).subscribe( categoriasAsociadas => {
-        this.subcategoriasAsociadas = catSubAsociadas[1];
-        for (var subcategoriaAsociada of this.subcategoriasAsociadas) {
-          subcategoriaAsociada.diccionarioId = this.nuevoDiccionario.id;
-        }
-
-        this.subGrmDicService.crear(this.subcategoriasAsociadas).subscribe(subcategoriasAsociadas => {
-          this.router.navigate(['pagina-principal']);
-        });
-      });
+      marcacion: this.formBuilder.group({
+        diatopica: this.formBuilder.array([]),
+        diacronica: this.formBuilder.array([]),
+        frecuencia: this.formBuilder.array([]),
+        valoracionSocial: this.formBuilder.array([]),
+        estratificacionSocial: this.formBuilder.array([]),
+        pragmatica: this.formBuilder.array([]),
+        diatecnica: this.formBuilder.array([]),
+      }),
     });
   }
 
-  unsubscribe() {
-    this.cdcServiceSubscription.unsubscribe();
-    this.ccaServiceSubscription.unsubscribe();
+  /* ------ Metodos para administrar las categorias y subcategorias ------ */
+
+  getCatGramaticales(): FormArray {
+    return this.diccionarioFormGroup.get('catGramaticales') as FormArray;
+  }
+
+  addCatGramatical() {
+    const catGramaticales = this.getCatGramaticales();
+
+    catGramaticales.push(this.formBuilder.group({
+      nombre: this.formBuilder.control(''),
+      abreviatura: this.formBuilder.control(''),
+      descripcion: this.formBuilder.control(''),
+      subGramaticales: this.formBuilder.array([]),
+    }));
+  }
+
+  getSubGramaticales(catGramatical: FormGroup): FormArray {
+    return catGramatical.get('subGramaticales') as FormArray;
+  }
+
+  addSubGramatical(catGramatical: FormGroup) {
+    const subGramaticales = this.getSubGramaticales(catGramatical);
+
+    subGramaticales.push(this.formBuilder.group({
+      nombre: this.formBuilder.control(''),
+      abreviatura: this.formBuilder.control(''),
+      descripcion: this.formBuilder.control(''),
+    }));
+  }
+
+  /* ------ Metodos para administrar la informacion ------ */
+
+  getInformacion(informacion): FormArray {
+    return this.diccionarioFormGroup.get('informacion').get(informacion) as FormArray;
+  }
+
+  addInformacion(informacion) {
+    const informacionArray = this.getInformacion(informacion);
+
+    informacionArray.push(this.formBuilder.group({
+      nombre: this.formBuilder.control(''),
+      descripcion: this.formBuilder.control(''),
+    }));
+  }
+
+  /* ------ Metodos para administrar las marcaciones ------ */
+
+  getMarcacion(marcacion): FormArray {
+    return this.diccionarioFormGroup.get('marcacion').get(marcacion) as FormArray;
+  }
+
+  addMarcacion(marcacion) {
+    const marcacionArray = this.getMarcacion(marcacion);
+
+    marcacionArray.push(this.formBuilder.group({
+      nombre: this.formBuilder.control(''),
+      descripcion: this.formBuilder.control(''),
+    }));
+  }
+
+  /* ----- Metodos para guardar en la base de datos ----- */
+
+  onSubmit() {
+    console.log(this.diccionarioFormGroup.value);
   }
 }
